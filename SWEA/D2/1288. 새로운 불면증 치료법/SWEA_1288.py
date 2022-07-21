ecfd185b6d17d9eed279b819a7041942c7d61235
wxxk <dwde22@gmail.com>
2022-07-22 00:51:44 +0900

    SWEA_1288

--- SWEA/D2/1288. 새로운 불면증 치료법/SWEA_1288.py
+++ SWEA/D2/1288. 새로운 불면증 치료법/SWEA_1288.py
@@ -11,8 +11,15 @@ for i in range(1, T+1):
         if 0 in list: # 리스트에 0이 있으면 number를 리스트
             a += 1
             num = str(number * a)
-            for j in range(len(num)):
+            # num = 1295
+            for j in range(len(num)): #num 숫자의 길이
+                # range(len(num)) = range(0,4)
+            
                 list[int(num[j])] = 1
+                # num[1] : 리스트 1번자리 = 1
+                # num[2] : 리스트 2번자리 = 1
+                # num[9] : 리스트 9번자리 = 1
+                # num[5] : 리스트 5번자리 = 1
         else:
             break
     print('#{} {}'.format(i, num))
