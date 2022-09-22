bdbfe55e6539b61ca0783a79522901fb5bbbe317
wxxk <dwde22@gmail.com>
2022-09-22 23:54:21 +0900

    test3

--- class/220922/project/django_0922/settings.py
+++ class/220922/project/django_0922/settings.py
@@ -20,22 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os, json
-from django.core.exceptions import ImproperlyConfigured
-
-secret_file = os.path.join(BASE_DIR, 'secrets.json') # secrets.json 파일 위치를 명시
-
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
-
-def get_secret(setting, secrets=secrets):
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
-
-SECRET_KEY = get_secret("SECRET_KEY")
+SECRET_KEY = 'django-insecure-=x_lect%49cliakask%!3h9z&g0b$nxaeqw-d06pe$al^nml1%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
